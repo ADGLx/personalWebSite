00c1b9d6ca8471b9c334f7fdc0df43a82b0beddd
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-01 15:04:13 -0500

    added favicon

--- LoginPage.php
+++ LoginPage.php
@@ -10,6 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="MyStyle.css">
    
+    <!--Favicon-->
+    <link rel="icon" href ="img/creature.gif">
     <!--Boostrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
