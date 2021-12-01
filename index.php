d1e710b5ed9731c0679ab3ab52383a4dd30941d3
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-01 15:12:41 -0500

    Added title

--- index.php
+++ index.php
@@ -41,8 +41,11 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
       <img src="/img/creature.gif" width="35" height="35" >
-      
-        <a class="navbar-brand" href="#">My Schedule</a>
+      <?php 
+        $temp = $_SESSION["userUsername"];
+          $temp = strtoupper($temp);
+      echo "<a class='navbar-brand' href='#'>$temp&#39;s Schedule</a> "?>
+        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
