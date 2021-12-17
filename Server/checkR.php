ad6fb06d03e7b97f58d23bfed96aff8f30ef99b8
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-17 14:32:10 -0500

    added the way to check reminders

--- Server/checkR.php
+++ Server/checkR.php
@@ -1,5 +1,6 @@
 <?php
 //This will be executed by the server every minute or so
+//This checks for the current reminders set 
  include "../includes/ConectSQL.php";
  $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
  fwrite($myfile, "this is a test");
