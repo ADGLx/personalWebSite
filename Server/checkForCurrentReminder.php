a6fd382e5a40401f136b169032f7e78291e82936
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-17 14:28:37 -0500

    Update checkForCurrentReminder.php

--- Server/checkForCurrentReminder.php
+++ Server/checkForCurrentReminder.php
@@ -1,6 +1,6 @@
 <?php
 //This will be executed by the server every minute or so
- include "includes ../includes/ConectSQL.php";
+ include "../includes/ConectSQL.php";
  $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
  fwrite($myfile, "this is a test");
  fclose($myfile);
