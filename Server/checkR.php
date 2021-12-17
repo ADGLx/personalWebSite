9998c5ae21299e247aa7894a74040ab4f66ce2b0
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-17 15:50:17 -0500

    Update checkR.php

--- Server/checkR.php
+++ Server/checkR.php
@@ -2,7 +2,27 @@
 //This will be executed by the server every minute or so
 //This checks for the current reminders set 
  include "../includes/ConectSQL.php";
- $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
- fwrite($myfile, "this is a test \n");
- fclose($myfile);
+ 
+ //$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+ //fwrite($myfile, "this is a test \n");
+ //fclose($myfile);
+
+ //This works now I can run the php file from the server now I just have to connect to the SQL and get all the info
+ $todaysDate = date("Y-m-d");
+ $sql = " SELECT * FROM `reminders` WHERE date = '$todaysDate' "; //This for now just gets today's date
+
+ //Now just send the email with the sql info
+
+ if($result = mysqli_query($conn, $sql))
+    {
+        //Grabs the result in an array and print them all 
+        while ($row = mysqli_fetch_assoc($result)) 
+        {
+            
+        }
+    } 
+
+    //Just send a test email here
+    mail("alvarodavidglx@hotmail.com", "Test 1", "This is my first test");
+
 ?>
