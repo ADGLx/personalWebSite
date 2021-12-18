292bfba752b5d5c67770fcca60011beea5a4a90b
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-18 12:53:33 -0500

    Update checkR.php

--- Server/checkR.php
+++ Server/checkR.php
@@ -1,7 +1,7 @@
 <?php
 //This will be executed by the server every minute or so
 //This checks for the current reminders set 
- include "./includes/ConectSQL.php";
+ include "/includes/ConectSQL.php";
  
  //$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
  //fwrite($myfile, "this is a test \n");
