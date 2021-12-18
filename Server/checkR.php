9c50059fcaabcf5bfa282ea7b1fe436e1714406c
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-18 14:18:34 -0500

    Update checkR.php

--- Server/checkR.php
+++ Server/checkR.php
@@ -16,32 +16,32 @@ $conn = mysqli_connect($dbServername, $dbUserName, $dbPassword, $dbName);
  //This works now I can run the php file from the server now I just have to connect to the SQL and get all the info
  $todaysDate = date("Y-m-d");
  $rn = date("h:i").":00"; //Ignore the seconds
+ $futureTime = date("h:i", strtotime("+15 minutes", strtotime($rn))).":00";
  //15 minutes in advance
- $sql = "SELECT * FROM `reminders` WHERE  time = ADDTIME('$todaysDate $rn','00:15:00') AND date = $todaysDate AND notify = true"; //This for now just gets today's date
+ $sql = "SELECT * FROM reminders WHERE  time = $futureTime AND date = $todaysDate AND notify = true"; //This for now just gets today's date
 
  //Now just send the email with the sql info
 
- echo "Attempting to get reminders at $rn on $todaysDate \n\n";
+ echo "Attempting to get reminders at $rn but that actually $futureTime on $todaysDate \n\n";
 
  if($result = mysqli_query($conn, $sql))
     {
-        echo "SQL Returned a result! \n";
+        //echo "SQL Returned a result! \n";
         //Grabs the result in an array and print them all 
         while ($row = mysqli_fetch_assoc($result)) 
         {
             echo "SQL Returned a row! Now attempting to find user ". $row["userid"] ."\n";
             //Now for each of the grabbed results of the sql gotta check the other table
-            $sql2 = "SELECT * FROM `logins` WHERE id ='" . $row["userid"]. "'";
+            $sql2 = "SELECT * FROM logins WHERE id ='" . $row["userid"]. "'";
 
                 if($result2 = mysqli_query($conn, $sql2))
                 {
                     echo "found reminder?\n";
-                    while($user = mysqli_fetch_assoc($result))
+                    if($user = mysqli_fetch_assoc($result2))
                     {
                         //This should execute only once so I will exit manually
+                        echo "sending email to ". $user["email"]. " title: ". $row["title"] . " description: " . $row["description"];
                         mail($user["email"],$row["title"], $row["description"]);
-                        echo "sent email to ". $user["email"]. " title: ". $row["title"] . " description: " . $row["description"];
-                        break;
                     }
                 }
                 else 
