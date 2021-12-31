e2da582c4e0cd4c0722569b441fb381adc52e5ac
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-12-31 15:00:49 -0500

    Quick fixes

--- LoginPage.php
+++ LoginPage.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="MyStyle.css">
+    <link rel="stylesheet" href="NewCSS.css">
    
     <!--Favicon-->
     <link rel="icon" href ="img/creature.png">
